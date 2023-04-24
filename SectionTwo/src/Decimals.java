2f7b28290d9eb7a986c278484f870bcc9014bd1f
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-04-24 16:16:35 -0300

    Adding the Decimals class that describes the type double in the Java

---
+++ SectionTwo/src/Decimals.java
@@ -0,0 +1,6 @@
+public class Decimals {
+    public static void main(String[] args) {
+        double percentege = 73.5 ;
+        System.out.println(percentege + " of percentages are male");
+    }
+}
