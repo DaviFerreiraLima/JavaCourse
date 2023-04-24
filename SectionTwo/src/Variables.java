2f0a2089d7334957879fbb90eb07cd645ac031f1
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-04-24 14:30:19 -0300

    Primeiro commit de variables

---
+++ SectionTwo/src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        int passangers = 5;
+        passangers ++;
+
+        System.out.println(passangers);
+
+        int busTickets = passangers;
+        System.out.println(busTickets);
+
+
+    }
+}
