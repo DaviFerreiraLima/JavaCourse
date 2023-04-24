6050128369ed07da19e6a0af7ba512da4b92dfaa
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-04-24 16:12:35 -0300

    Adding the WholeNumbers class that describes the type long in the Java

---
+++ SectionTwo/src/WholeNumbers.java
@@ -0,0 +1,9 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        long  globalPopulation = 8_000_000_000L;
+        long dailyGoogleSearches = 8_500_000_000L;
+
+        System.out.println("Global population is "+globalPopulation);
+        System.out.println("The daily google serches are :"+dailyGoogleSearches);
+    }
+}
