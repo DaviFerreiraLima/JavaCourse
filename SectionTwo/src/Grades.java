014eabe046efe14c8fd956cb5f71bf139e8225f9
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-04-24 15:40:30 -0300

    Adding the Char class that describes the type char in the Java

---
+++ SectionTwo/src/Grades.java
@@ -0,0 +1,10 @@
+public class Grades {
+    public static void main(String[] args) {
+        char potions = 'A';
+        char arithmancy = 'B';
+        char charms = 'C';
+
+        System.out.println("Pontions "+ potions + "\nArithmancy "+ arithmancy + "\nCharms "+charms );
+
+    }
+}
