15b1b157666719741acfac8278a24a11f10cc928
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-04-25 17:51:55 -0300

    Adding the function section

---
+++ SectionEight/src/Chorus.java
@@ -0,0 +1,16 @@
+public class Chorus {
+    public static void main(String[] args) {
+        singChorus();
+
+    }
+    static void singChorus(){
+        for (int i = 0; i<5 ;i++ ){
+            System.out.println("singing");
+            System.out.println("Dont blame it on the sunshine");
+            System.out.println("Dont blame it on the moonlight");
+            System.out.println("Dont blame it on good times");
+            System.out.println("Dont blame it on the boogie \n");
+        }
+
+    }
+}
