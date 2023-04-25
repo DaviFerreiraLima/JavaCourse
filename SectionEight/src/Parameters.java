396964c549a1f788bb4f817030fa0e88fff6be6b
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-04-25 18:28:34 -0300

    Adding the Parameters file

---
+++ SectionEight/src/Parameters.java
@@ -0,0 +1,9 @@
+public class Parameters {
+    public static void main(String[] args) {
+        calculateArea(2,2);
+    }
+    public  static void calculateArea(double length, double width){
+        double area = length*width;
+        System.out.println("The Area is: " + area);
+    }
+}
