f4180a3a40831660bfb70d7fadbba721b50ed642
Davi Ferreira Lima <daviferreilima@gmail.com>
2023-04-26 10:10:55 -0300

    Adding the documentation from the return function

--- SectionEight/src/DocComments.java
+++ SectionEight/src/DocComments.java
@@ -5,25 +5,40 @@ public class DocComments {
 
     /**
      * Function name : greet
-     *
+     * <p>
      * Inside the function
      * 1. prints: " Hi "
-     *
      */
-    public static void greet(){
+    public static void greet() {
         System.out.println("Hi");
     }
 
     /**
      * Function name : printText
+     *
      * @param name (String)
-     * @param age (String)
+     * @param age  (String)
+     *
+     *         Inside the function:
+     *            1.prints the name and the age as part of the text
+     */
+    public static void printText(String name, String age) {
+        System.out.println("Hi, I'm " + name + " and i im " + age + " years old.");
+    }
+
+    /**
+     * Function name : CalculateArea
+     * @param length (double)
+     * @param width  (double)
+     * @return       (double)
      *
      * Inside the function:
-     *  1.prints the name and the age as part of the text
+     *  1. calculates the area and return it.
      *
      */
-    public static void printText(String name , String age){
-        System.out.println("Hi, I'm "+ name + " and i im "+age + " years old.");
+    public static double calculateArea(double length, double width) {
+
+        double area = length * width;
+        return area;
     }
 }
